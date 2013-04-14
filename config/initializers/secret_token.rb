1d817facb0703c6cd7f9a7e466324d419a44ed2d
Simon Hards <simon_hards@hotmail.com>
2013-04-14 17:49:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradeServer::Application.config.secret_token = '917464201ad9cc6364349fa2e00d5d06ede7e33e81299c2336d42278c70cc37c21e66d4e9fdedb455fdf6b17a7a674652e13ce88d25ee6d153ab0ae1ade5e16c'
